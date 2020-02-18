531438a418d016739f15e68fc61dd57585d22840
Eivind <58335786+eico6@users.noreply.github.com>
2020-02-18 15:13:53 +0100

    Steinen Testen
    - Test stein for merge testing

---
+++ Source/Light_Prototype/Steinen.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Steinen.generated.h"
+
+UCLASS()
+class LIGHT_PROTOTYPE_API ASteinen : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASteinen();
+
+	UPROPERTY(VisibleAnywhere, Category = "Mesh")
+	class UStaticMeshComponent* Mesh;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
