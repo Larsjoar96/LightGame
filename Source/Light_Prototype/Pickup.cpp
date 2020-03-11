0cadd903508f0ee25bff015ae411f0e34b6da31c
Larsjoar96 <32615350+Larsjoar96@users.noreply.github.com>
2020-03-11 21:32:24 +0100

    Added files back in

---
+++ Source/Light_Prototype/Pickup.cpp
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pickup.h"
+#include "Components/SceneComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values
+APickup::APickup()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Create a default root component (null object)
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+
+	// Create a mesh for the pickup
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(GetRootComponent());
+
+	// Create a hitbox for the pickup
+	Collider = CreateDefaultSubobject<UBoxComponent>(TEXT("Collider"));
+	Collider->SetupAttachment(GetRootComponent());
+
+	// Decides which type of objects it will react with, and if it should overlap or collide.
+	Collider->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	Collider->SetCollisionObjectType(ECollisionChannel::ECC_WorldStatic);
+	Collider->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	Collider->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+
+	// Mesh got its own collision, we don't want to control any functionalities through that collision
+	Mesh->SetCollisionObjectType(ECollisionChannel::ECC_WorldStatic);
+	Mesh->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+}
+
+// Called when the game starts or when spawned
+void APickup::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	// Start checking for OverlapBegin events
+	Collider->OnComponentBeginOverlap.AddDynamic(this, &APickup::OnOverlapBegin);
+}
+
+// Called every frame
+void APickup::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void APickup::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+							 int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("PARENT: OnOverlapBegin called"))
+
+	// Put particle effects and sound effects here (as it's common amongst all pickups)
+}
