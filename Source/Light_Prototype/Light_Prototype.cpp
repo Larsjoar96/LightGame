84beacf2d9b406564bcdafc3def27906e6a6cea8
Larsjoar96 <32615350+Larsjoar96@users.noreply.github.com>
2020-03-11 21:31:55 +0100

    Delete all files to reset

--- Source/Light_Prototype/Light_Prototype.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Light_Prototype.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Light_Prototype, "Light_Prototype" );
