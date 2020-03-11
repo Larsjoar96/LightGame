77700d159dbc3b224483952202c90040f4ce3f90
Larsjoar96 <32615350+Larsjoar96@users.noreply.github.com>
2020-03-11 20:51:31 +0100

    Revert "Merge branch 'Eivind'"
    This reverts commit e62dc56340c6eb7e8338d1ab788d8374ef8355ab, reversing
    changes made to 0cf076d88fb6029f7559897353d726b71d12cc06.

--- Source/Light_Prototype/EnemySpawner.h
+++ Source/Light_Prototype/EnemySpawner.h
@@ -9,20 +9,6 @@
 #include "EnemySpawner.generated.h"
 
 
-// ENUM identical to the ENUM in 'Enemy.h' where the these spawners will check if an enemy instance
-// is assigned the same integer value (same label). If identical: the enemy should be spawned by this spawner.
-UENUM(BlueprintType)
-enum class ESpawnLabel : uint8
-{
-	ESL_None	    UMETA(Displayname = "None"),
-	ESL_Label_One	UMETA(Displayname = "Label_One"),
-	ESL_LABEL_Two   UMETA(Displayname = "Label_Two"),
-	ESL_Label_Three UMETA(Displayname = "Label_Three"),
-	ESL_Label_Four  UMETA(Displayname = "Label_Four"),
-	ESL_Max		    UMETA(Displayname = "DefaultMax")
-};
-
-
 UCLASS()
 class LIGHT_PROTOTYPE_API AEnemySpawner : public AActor
 {
@@ -38,9 +24,8 @@ public:
 	UPROPERTY(EditAnywhere, Category = "Visual of Points")
 	class UStaticMeshComponent* StartPoint;
 
-	// What group of enemies this spawner should spawn. Assignment is made in editor.
-	UPROPERTY(EditAnywhere, Category = "Enums")
-	ESpawnLabel SpawnLabel;
+	//UPROPERTY(EditAnywhere, Category = "Distance")
+	//float TravelDistance;
 
 	// Array filled up with instances to spawn
 	TArray<AEnemy*> Enemies;
@@ -48,17 +33,9 @@ public:
 	// TimerHandle to track time processes
 	FTimerHandle MyTimerHandle;
 
-	int32 MaxIndex;
-
-	int32 CurrentWave;
-
 	int32 CurrentIndex;
 
-	// Spawners are limited to 3 waves for now...
-	UPROPERTY(EditAnywhere, Category = "Waves")
-	int32 Waves[3];
-
-	float Seconds; // ONLY DEBUGGING!
+	int32 MaxIndex;
 
 protected:
 	// Called when the game starts or when spawned
@@ -70,7 +47,5 @@ public:
 
 	// Spawn next enemy in line
 	void SpawnEnemies();
-
-	void SpawnNextWave();
 
 };
